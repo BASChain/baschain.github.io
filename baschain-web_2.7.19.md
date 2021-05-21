8d70b4d5b76ca25311003ae61789de281e12a237
developer <team-developer@gmail.com>
2021-05-21 11:07:39 +0800

    upd: about

---
+++ baschain-web_2.7.19.md
@@ -0,0 +1,2 @@
+baschain-web : 2.7.19 
+ Build at 2021-05-21 10:56:54
---
+++ static/js/35.a69d548d5486040d38bc.js
@@ -0,0 +1,2 @@
+webpackJsonp([35],{"85UH":function(e,t,l){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var n={render:function(){var e=this.$createElement;return(this._self._c||e)("el-collapse",{staticClass:"bas-el-collapse"})},staticRenderFns:[]};var s=l("VU/8")({name:"SellerIssueNav"},n,!1,function(e){l("JroO")},null,null);t.default=s.exports},JroO:function(e,t){}});
+//# sourceMappingURL=35.a69d548d5486040d38bc.js.map
